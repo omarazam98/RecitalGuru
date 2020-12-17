9080e4522ec936c778bd5b0230fa47b2513e6a96
Omar Azam <omarazam@Omars-Air.hitronhub.home>
2020-12-17 14:01:50 -0700

    Updates

---
+++ precache-manifest.9e024b46f117cfccf0122a4bc1604de0.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d417a59fe50c2851b04eeffa035c346c",
+    "url": "/RecitalGuru/index.html"
+  },
+  {
+    "revision": "3a27157bc642bd9b24fa",
+    "url": "/RecitalGuru/static/css/4.4f5fa3e4.chunk.css"
+  },
+  {
+    "revision": "c8aac1f139f3e296ffcb",
+    "url": "/RecitalGuru/static/css/main.48261d22.chunk.css"
+  },
+  {
+    "revision": "20f4fc6a2ec02c04c11d",
+    "url": "/RecitalGuru/static/js/0.39e7fef5.chunk.js"
+  },
+  {
+    "revision": "54df4d02a7c5f0017e77",
+    "url": "/RecitalGuru/static/js/10.10d3472b.chunk.js"
+  },
+  {
+    "revision": "f562097f8c2a5b5e785a",
+    "url": "/RecitalGuru/static/js/11.6920c0c8.chunk.js"
+  },
+  {
+    "revision": "4114207469fe9aaa742b",
+    "url": "/RecitalGuru/static/js/12.46f0e217.chunk.js"
+  },
+  {
+    "revision": "269331dbd7fab9b42a7d",
+    "url": "/RecitalGuru/static/js/13.ccdbe4a5.chunk.js"
+  },
+  {
+    "revision": "062a70bc234b958a58ae",
+    "url": "/RecitalGuru/static/js/14.0bf88f0f.chunk.js"
+  },
+  {
+    "revision": "414ebaa0c49017829a27",
+    "url": "/RecitalGuru/static/js/15.30a76166.chunk.js"
+  },
+  {
+    "revision": "133fd54ccee0d6966236",
+    "url": "/RecitalGuru/static/js/16.6cadcf40.chunk.js"
+  },
+  {
+    "revision": "0b5d1a0c0fcb53035e6a",
+    "url": "/RecitalGuru/static/js/17.c593fa15.chunk.js"
+  },
+  {
+    "revision": "6619b5b309d1133262d2",
+    "url": "/RecitalGuru/static/js/18.0e1a8b68.chunk.js"
+  },
+  {
+    "revision": "6d3b5094a03da39cda84",
+    "url": "/RecitalGuru/static/js/19.bdc6a162.chunk.js"
+  },
+  {
+    "revision": "65565aab4d22812ee3b5",
+    "url": "/RecitalGuru/static/js/20.e4ddd8fa.chunk.js"
+  },
+  {
+    "revision": "e80e2d2444db9c212afe",
+    "url": "/RecitalGuru/static/js/21.882aaebf.chunk.js"
+  },
+  {
+    "revision": "8b45884afce72a0c0d6b",
+    "url": "/RecitalGuru/static/js/22.a5778a34.chunk.js"
+  },
+  {
+    "revision": "bf3abf0aed4ad96270f6",
+    "url": "/RecitalGuru/static/js/23.e81664e4.chunk.js"
+  },
+  {
+    "revision": "3d91124980c5d6ead501",
+    "url": "/RecitalGuru/static/js/24.ab000a85.chunk.js"
+  },
+  {
+    "revision": "6fbd5f764f47369dfbc2",
+    "url": "/RecitalGuru/static/js/25.643c46a5.chunk.js"
+  },
+  {
+    "revision": "68d7ffe5ddce9ec76ee1",
+    "url": "/RecitalGuru/static/js/26.f22f550d.chunk.js"
+  },
+  {
+    "revision": "36a1f02e3b52f3a4494a",
+    "url": "/RecitalGuru/static/js/27.11be509d.chunk.js"
+  },
+  {
+    "revision": "7d7fb2aa2b2e036d7ca8",
+    "url": "/RecitalGuru/static/js/28.d3c9133a.chunk.js"
+  },
+  {
+    "revision": "d494fd411680e86cda42",
+    "url": "/RecitalGuru/static/js/29.7bdcf0d7.chunk.js"
+  },
+  {
+    "revision": "6f8c83b59b96d6ddc5c3",
+    "url": "/RecitalGuru/static/js/30.dffa666b.chunk.js"
+  },
+  {
+    "revision": "86efc77f4d6bbd468cda",
+    "url": "/RecitalGuru/static/js/31.bc7e6cee.chunk.js"
+  },
+  {
+    "revision": "4f24dd9430197db3d70a",
+    "url": "/RecitalGuru/static/js/32.f65b193e.chunk.js"
+  },
+  {
+    "revision": "d9fe235e9b233d9aaa00",
+    "url": "/RecitalGuru/static/js/33.af723156.chunk.js"
+  },
+  {
+    "revision": "d4f889e1ca1d52768c5f",
+    "url": "/RecitalGuru/static/js/34.851716cc.chunk.js"
+  },
+  {
+    "revision": "1a79cc4e8d825081c6d1",
+    "url": "/RecitalGuru/static/js/35.5aab2c23.chunk.js"
+  },
+  {
+    "revision": "76623119203cf9fc57eb",
+    "url": "/RecitalGuru/static/js/36.847ec5ab.chunk.js"
+  },
+  {
+    "revision": "9851b8a2ec223bba4d43",
+    "url": "/RecitalGuru/static/js/37.3d6bee8f.chunk.js"
+  },
+  {
+    "revision": "d4883aaa52781e96f214",
+    "url": "/RecitalGuru/static/js/38.64b3dbf0.chunk.js"
+  },
+  {
+    "revision": "f7bde849fbe02558a2ee",
+    "url": "/RecitalGuru/static/js/39.d53fa70d.chunk.js"
+  },
+  {
+    "revision": "3a27157bc642bd9b24fa",
+    "url": "/RecitalGuru/static/js/4.357bc33b.chunk.js"
+  },
+  {
+    "revision": "29eeb0a344d7c3d4902617c4543f9a94",
+    "url": "/RecitalGuru/static/js/4.357bc33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa72698e704a849d06f3",
+    "url": "/RecitalGuru/static/js/40.8e06bb4f.chunk.js"
+  },
+  {
+    "revision": "d610f139356e6f5ecd56",
+    "url": "/RecitalGuru/static/js/41.f27e78ec.chunk.js"
+  },
+  {
+    "revision": "5c852b5417e03707f948",
+    "url": "/RecitalGuru/static/js/42.482119a2.chunk.js"
+  },
+  {
+    "revision": "773294a2d3dfb9887fe8",
+    "url": "/RecitalGuru/static/js/43.b7e1d7da.chunk.js"
+  },
+  {
+    "revision": "879694c627f796d9a5c9",
+    "url": "/RecitalGuru/static/js/44.7ab6f029.chunk.js"
+  },
+  {
+    "revision": "1fb1b811c3cb61335e35",
+    "url": "/RecitalGuru/static/js/45.145dc0b6.chunk.js"
+  },
+  {
+    "revision": "10833c5c390fbffa9416",
+    "url": "/RecitalGuru/static/js/46.7a37cb97.chunk.js"
+  },
+  {
+    "revision": "57e9e2817638bbe8dd2e",
+    "url": "/RecitalGuru/static/js/47.b68d0023.chunk.js"
+  },
+  {
+    "revision": "c6f19322bff61534b38f",
+    "url": "/RecitalGuru/static/js/48.b0d0c66a.chunk.js"
+  },
+  {
+    "revision": "4a5fb826dbd1ace96589",
+    "url": "/RecitalGuru/static/js/49.a14718d3.chunk.js"
+  },
+  {
+    "revision": "f46ca1b4737d23cd9328",
+    "url": "/RecitalGuru/static/js/5.633262ba.chunk.js"
+  },
+  {
+    "revision": "25cb8750baa2456e8faa",
+    "url": "/RecitalGuru/static/js/50.6103d4df.chunk.js"
+  },
+  {
+    "revision": "cf4ce35fd1a43f563ace",
+    "url": "/RecitalGuru/static/js/51.a05875a7.chunk.js"
+  },
+  {
+    "revision": "a6176cd623c8d9b5b5f4",
+    "url": "/RecitalGuru/static/js/52.14724a02.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/RecitalGuru/static/js/52.14724a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae4a35de03977ee3ce9",
+    "url": "/RecitalGuru/static/js/53.262d2651.chunk.js"
+  },
+  {
+    "revision": "387725d59a0ec7d1dd46",
+    "url": "/RecitalGuru/static/js/54.c2f62f08.chunk.js"
+  },
+  {
+    "revision": "ca6149f37bc57df94ece",
+    "url": "/RecitalGuru/static/js/55.44727f5b.chunk.js"
+  },
+  {
+    "revision": "7d9e237d9ff781661dbc",
+    "url": "/RecitalGuru/static/js/56.6489e737.chunk.js"
+  },
+  {
+    "revision": "ea76e8ada121693241f3",
+    "url": "/RecitalGuru/static/js/57.4f897533.chunk.js"
+  },
+  {
+    "revision": "925c57a09c000f9fde15",
+    "url": "/RecitalGuru/static/js/58.2546630b.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/RecitalGuru/static/js/58.2546630b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4980bf3e5a78f5073365",
+    "url": "/RecitalGuru/static/js/6.e269649a.chunk.js"
+  },
+  {
+    "revision": "f6863217410e891ef88b",
+    "url": "/RecitalGuru/static/js/7.40ec4d72.chunk.js"
+  },
+  {
+    "revision": "0a0f4a6e3c61a668873e",
+    "url": "/RecitalGuru/static/js/8.bb00f4f8.chunk.js"
+  },
+  {
+    "revision": "b71b2e8612d370769101",
+    "url": "/RecitalGuru/static/js/9.578c88a0.chunk.js"
+  },
+  {
+    "revision": "c8aac1f139f3e296ffcb",
+    "url": "/RecitalGuru/static/js/main.ddc899f7.chunk.js"
+  },
+  {
+    "revision": "e4c90de3435f3f730091",
+    "url": "/RecitalGuru/static/js/polyfills-dom.caad0738.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/RecitalGuru/static/js/polyfills-dom.caad0738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637a2bf606edb1be9023",
+    "url": "/RecitalGuru/static/js/runtime-main.cfb3f94d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RecitalGuru/precache-manifest.cd8481f2fa988ec797d2e80433487e7f.js"
+  "/RecitalGuru/precache-manifest.9e024b46f117cfccf0122a4bc1604de0.js"
 );
 
 self.addEventListener('message', (event) => {
