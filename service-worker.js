40bfcb49948cce2680ddc28b413002ce216f94bc
Omar Azam <omarazam@Omars-Air.hitronhub.home>
2020-12-19 16:50:37 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RecitalGuru/precache-manifest.9e024b46f117cfccf0122a4bc1604de0.js"
+  "/RecitalGuru/precache-manifest.3ce55959918a990b8a1bcd249813196c.js"
 );
 
 self.addEventListener('message', (event) => {
