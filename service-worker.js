a736bd290b85ff08b0ed9b78a04142aa30c8d051
omarazam98 <umarazam98@gmail.com>
2021-03-18 14:03:52 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RecitalGuru/precache-manifest.f5af8a01d7d861fe60ab121e1dfaf82f.js"
+  "/RecitalGuru/precache-manifest.0362e55c5a60e4f932bd5c4d4caabb87.js"
 );
 
 self.addEventListener('message', (event) => {
