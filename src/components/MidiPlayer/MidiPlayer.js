bd4d4cb2b91690be51b186e25478540e248e54f6
omarazam98 <umarazam98@gmail.com>
2021-04-14 00:44:54 -0400

    revert

--- src/components/MidiPlayer/MidiPlayer.js
+++ src/components/MidiPlayer/MidiPlayer.js
@@ -236,7 +236,7 @@ export const MidiPlayer = async (ac, soundfont, data, freqRef, practice, swiper,
         } else {
             setTimeout(() => {
                 check.current = false;
-            }, 60)
+            }, 66)
         }
     })
 
